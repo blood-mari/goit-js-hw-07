9c74db17d6f615f9c1226f0b18a4aa8a5a3b0c66
Maria <mashero4ka@gmail.com>
2020-10-07 20:43:11 +0300

    fixed mistakes 6th task

--- js/task-6.js
+++ js/task-6.js
@@ -9,7 +9,7 @@
 const inputRef = document.querySelector('#validation-input');
 
 const onInputFocusChange = () => {
-    if (inputRef.value.length >= inputRef.dataset.length) {
+    if (inputRef.value.length === inputRef.dataset.length) {
         inputRef.classList.add('valid');
         inputRef.classList.remove('invalid');
     }
